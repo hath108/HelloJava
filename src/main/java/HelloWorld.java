3d88afc24bd908950297904ddb95425b85fae948
hath108 <sbksbk@ya.ru>
2020-06-07 17:47:54 +0300

    tests added

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        printLines();
+    }
+    public static void printText(String string){
+        System.out.println(string);
+    }
+
+    public static void printLines(){
         System.out.println("Hello World and Java!");
         System.out.println("Second line");
     }
