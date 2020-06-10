8891281fee32227465884dc0bfb63990a787f7a9
hath108 <sbksbk@ya.ru>
2020-06-10 12:40:59 +0300

    ready to work :)

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,14 +1,14 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        printLines();
-    }
+
     public static void printText(String string){
         System.out.println(string);
     }
 
-    public static void printLines(){
-        System.out.println("Hello World and Java!");
-        System.out.println("Second line");
-        System.out.println("print lines");
+    public static int getLucky(){
+        return 7;
+    }
+
+    public static String getHelloWorld(){
+        return "Hello World";
     }
 }
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,16 +1,27 @@
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * mvn test (запускаем все тесты)
+ * mvn -Dtest=HelloWorldTest test (запускаем все тесты в классе)
+ * mvn -Dtest=HelloWorldTest#printTextTest test (запускаем конкретный тест)
+ */
 class HelloWorldTest {
 
     @Test
-    void printText() {
-        HelloWorld.printText("Привет тестировщикам");
+    void getLuckyTest() {
+        System.out.println("getLuckyTest");
+        assertEquals(7, HelloWorld.getLucky(), "number is different");
     }
 
     @Test
-    void printLines() {
-        HelloWorld.printLines();;
+    void getHelloWorldTest(){
+        assertEquals("Hello World", HelloWorld.getHelloWorld(), "text is Different");
+    }
+    @Test
+    void printTextTest() {
+        HelloWorld.printText("Hello everybody");
     }
 }
